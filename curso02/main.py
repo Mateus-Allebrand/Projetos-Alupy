99ce0f684e5ced3d6a2f88c179896a3e0df83632
Mateus <125222236+Mateus-Allebrand@users.noreply.github.com>
2024-04-13 15:48:36 -0300

    str python

---
+++ curso02/main.py
@@ -0,0 +1,15 @@
+url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+
+print(url)
+
+# texto = url[0:5]
+# texto02 = url.split("/")
+# print(texto)
+# print(texto02)
+
+nome = "Gabriel saldanha"
+
+
+first_nome = nome.split(" ")
+
+print(first_nome[0])
